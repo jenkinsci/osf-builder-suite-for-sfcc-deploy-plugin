6683afc72a210873ebeefef15e4682dcd066ea05
Daniel Anechitoaie <daniel.anechitoaie@gmail.com>
2017-09-23 20:04:43 +0300

    Started refactoring code to use AbortException to stop execution

--- src/main/java/org/jenkinsci/plugins/osfbuildersuiteforsfcc/deploy/DeployBuilder.java
+++ src/main/java/org/jenkinsci/plugins/osfbuildersuiteforsfcc/deploy/DeployBuilder.java
@@ -5,6 +5,7 @@ import com.cloudbees.plugins.credentials.common.StandardCredentials;
 import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
 import com.cloudbees.plugins.credentials.domains.URIRequirementBuilder;
+import hudson.AbortException;
 import hudson.Launcher;
 import hudson.Extension;
 import hudson.FilePath;
@@ -204,8 +205,10 @@ public class DeployBuilder extends Builder implements SimpleBuildStep {
     }
 
     @Override
-    public void perform(@Nonnull Run<?, ?> build, @Nonnull FilePath workspace,
-                        @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws IOException {
+    public void perform(@Nonnull Run<?, ?> build,
+                        @Nonnull FilePath workspace,
+                        @Nonnull Launcher launcher,
+                        @Nonnull TaskListener listener) throws IOException, InterruptedException {
 
         PrintStream logger = listener.getLogger();
 
@@ -213,67 +216,65 @@ public class DeployBuilder extends Builder implements SimpleBuildStep {
         logger.println(String.format("--[B: %s]--", getDescriptor().getDisplayName()));
         logger.println();
 
-        try {
-            StandardUsernamePasswordCredentials bmCredentials = null;
-            if (StringUtils.isNotEmpty(bmCredentialsId)) {
-                bmCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.findCredentialById(
-                        bmCredentialsId,
-                        StandardUsernamePasswordCredentials.class,
-                        build, URIRequirementBuilder.create().build()
-                );
-            }
-
-            if (bmCredentials != null) {
-                com.cloudbees.plugins.credentials.CredentialsProvider.track(build, bmCredentials);
-            }
-
-            TwoFactorAuthCredentials tfCredentials = null;
-            if (StringUtils.isNotEmpty(tfCredentialsId)) {
-                tfCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.findCredentialById(
-                        tfCredentialsId,
-                        TwoFactorAuthCredentials.class,
-                        build, URIRequirementBuilder.create().build()
-                );
-            }
-
-            if (tfCredentials != null) {
-                com.cloudbees.plugins.credentials.CredentialsProvider.track(build, tfCredentials);
-            }
-
-            DeployCallable deployCallable = new DeployCallable(
-                    workspace,
-                    listener,
-                    hostname,
+        StandardUsernamePasswordCredentials bmCredentials = null;
+        if (StringUtils.isNotEmpty(bmCredentialsId)) {
+            bmCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.findCredentialById(
                     bmCredentialsId,
-                    bmCredentials,
-                    tfCredentialsId,
-                    tfCredentials,
-                    TokenMacro.expandAll(build, workspace, listener, buildVersion),
-                    getBuildCause(build),
-                    build.getNumber(),
-                    createBuildInfoCartridge,
-                    activateBuild,
-                    sourcePaths,
-                    tempDirectory,
-                    getDescriptor().getHttpProxyHost(),
-                    getDescriptor().getHttpProxyPort(),
-                    getDescriptor().getHttpProxyUsername(),
-                    getDescriptor().getHttpProxyPassword(),
-                    getDescriptor().getDisableSSLValidation()
+                    StandardUsernamePasswordCredentials.class,
+                    build, URIRequirementBuilder.create().build()
             );
+        }
 
-            if (!launcher.getChannel().call(deployCallable)) {
-                build.setResult(Result.FAILURE);
-            }
+        if (bmCredentials != null) {
+            com.cloudbees.plugins.credentials.CredentialsProvider.track(build, bmCredentials);
+        }
+
+        TwoFactorAuthCredentials tfCredentials = null;
+        if (StringUtils.isNotEmpty(tfCredentialsId)) {
+            tfCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.findCredentialById(
+                    tfCredentialsId,
+                    TwoFactorAuthCredentials.class,
+                    build, URIRequirementBuilder.create().build()
+            );
+        }
+
+        if (tfCredentials != null) {
+            com.cloudbees.plugins.credentials.CredentialsProvider.track(build, tfCredentials);
+        }
+
+        String expandedBuildVersion;
+        try {
+            expandedBuildVersion = TokenMacro.expandAll(build, workspace, listener, buildVersion);
         } catch (MacroEvaluationException e) {
-            logger.println();
-            logger.println("ERROR: Exception thrown while expanding build version!");
-            e.printStackTrace(logger);
-            logger.println();
+            AbortException abortException = new AbortException("Exception thrown while expanding build version!");
+            abortException.initCause(e);
+            throw abortException;
+        }
 
+        DeployCallable deployCallable = new DeployCallable(
+                workspace,
+                listener,
+                hostname,
+                bmCredentialsId,
+                bmCredentials,
+                tfCredentialsId,
+                tfCredentials,
+                expandedBuildVersion,
+                getBuildCause(build),
+                build.getNumber(),
+                createBuildInfoCartridge,
+                activateBuild,
+                sourcePaths,
+                tempDirectory,
+                getDescriptor().getHttpProxyHost(),
+                getDescriptor().getHttpProxyPort(),
+                getDescriptor().getHttpProxyUsername(),
+                getDescriptor().getHttpProxyPassword(),
+                getDescriptor().getDisableSSLValidation()
+        );
+
+        if (!launcher.getChannel().call(deployCallable)) {
             build.setResult(Result.FAILURE);
-        } catch (InterruptedException e) {
-            build.setResult(Result.ABORTED);
         }
 
         logger.println();
